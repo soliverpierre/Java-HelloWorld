52cc9bb4c36713172956ad87dfa9cd612069e2a7
soliver2023 <soliverpierre@gmail.com>
2024-11-21 11:30:51 -0600

    my first changes

---
+++ src/main/java/Marthe/Main.java
@@ -0,0 +1,21 @@
+package Marthe;
+
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+       // Single-line comment in Java
+
+        /*
+        * Multi_line comment in Java
+        *
+        *
+        * */
+
+        // System.out.println("Hello world!");
+        // JOptionPane.showMessageDialog(null, "This is a graphical Output");
+
+        System.out.println("Your random is: " + (1 + (int) (Math.random() * 10)));
+    }
+}
